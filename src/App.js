60d1146a83ef904166b688acac917c47945fa2b3
donta514 <dnovsmith.ds20@gmail.com>
2023-04-10 15:01:42 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div className="App">
+      <section className="side-bar">
+        <button> + New Chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Donta</p>
+        </nav>
+      </section>
+      <section className="main"></section>
+    </div>
+  );
+};
+
+export default App;
